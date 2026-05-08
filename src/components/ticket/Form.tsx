18b64ad5350ab453397aaa8c284457bb63a6ce9e
Jazmín Hernández <li.jhernandezz@gmail.com>
2026-05-08 12:43:07 -0600

    feat: compose Form component using UploadInput and TextInput UI elements

--- src/components/ticket/Form.tsx
+++ src/components/ticket/Form.tsx
@@ -4,18 +4,21 @@ import { Button } from '../ui/Button';
 
 export const Form = () => {
   return (
-    <div className="">
+    <form className="space-y-6">
       <UploadInput />
-      <div className="flex flex-col gap-3">
+
+      <div className="flex flex-col gap-4">
         <TextInput label="Full Name" placeholder="Jazmín Hernández" />
         <TextInput
           label="Email Address"
-          placeholder="jazminhernandez@gmail.com"
+          placeholder="example@gmail.com"
           type="email"
         />
-        <TextInput label="Instagram Username" placeholder="@jazminhernandez" />
+        <TextInput label="Instagram Username" placeholder="@youruser" />
       </div>
-      <Button />
-    </div>
+      <div className="pt-4">
+        <Button />
+      </div>
+    </form>
   );
 };
